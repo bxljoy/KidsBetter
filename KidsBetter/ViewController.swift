8f7995c828a025d80f30f22671b6ee502ad310fc
AlexBao <259500313@qq.com>
2021-12-26 22:31:04 +0800

    Initial Commit

---
+++ KidsBetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsBetter
+//
+//  Created by bxl on 2021/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
